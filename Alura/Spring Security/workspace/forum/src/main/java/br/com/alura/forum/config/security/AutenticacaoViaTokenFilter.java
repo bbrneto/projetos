8dd7fe452743cc22ab768e28c45d1af7a6e03b36
Benedito Barbosa <bbrneto@gmail.com>
2019-08-21 22:03:12 -0300

    Spring Security - Autenticação via token

---
+++ Alura/Spring Security/workspace/forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,61 @@
+package br.com.alura.forum.config.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.alura.forum.modelo.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+	
+	private TokenService tokenService;
+	
+	private UsuarioRepository usuarioRepository;
+
+	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+		this.tokenService = tokenService;
+		this.usuarioRepository = usuarioRepository;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+		String token = this.recuperaToken(request);
+		
+		boolean valido = this.tokenService.isTokenValido(token);
+		
+		if (valido) {
+			this.autenticaCliente(token);
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+
+	private String recuperaToken(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");
+		
+		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+			return null;
+		}
+		
+		return token.substring(7, token.length());
+	}
+
+	private void autenticaCliente(String token) {
+		Long idUsuario = this.tokenService.getIdUsuario(token);
+		
+		Usuario usuario = this.usuarioRepository.findById(idUsuario).get();
+		
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+
+}
