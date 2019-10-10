6d1cb1fe688b9fcaac1193f0cbf0bc7b867b94d9
Benedito Barbosa <bbrneto@gmail.com>
2019-10-10 18:58:34 -0300

    Inclusão da pasta Android

---
+++ Alura/Android/Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,29 @@
+package br.com.alura.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.alura.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        setTitle("Lista de alunos");
+
+        List<String> alunos = Arrays.asList("Alex", "Fran", "José", "Maria", "Ana");
+
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+    }
+}
